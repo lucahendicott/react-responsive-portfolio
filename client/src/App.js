4ffb34a902f6b0f19779112d785549993d0fcec8
Lucah Endicott <lucahdavid88@gmail.com>
2021-03-05 20:37:52 -0800

    update about me photo styles

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,28 @@ import About from "./Pages/About";
 import Portfolio from "./Pages/Portfolio";
 import Nav from "./Components/Nav";
 import Header from "./Components/Header";
+import Particles from "react-particles-js";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Particles
+          params={{
+            particles: {
+              line_linked: {
+                shadow: {
+                  enable: true,
+                  color: "#3CA9D1",
+                  blur: 1000,
+                },
+              },
+            },
+          }}
+          style={{
+            width: "100%",
+          }}
+        />
         <Nav />
         <Header />
         <Switch>
